3c3a4358407e1e11b029f3f50f7787c26c8db803
WuYiming <yimingwu@hotmail.com>
2017-03-17 14:44:55 +0800

    modify the network architecture
    1. use deepprior as the baseline
    2. add feature concatenation
    3. add lstm
    4. add multi branches
    5. delete the model based on ICIP paper

--- experiments/NYU/train_baseline_deepprior_concate_features.sh
+++ experiments/NYU/train_baseline_deepprior_concate_features.sh
@@ -7,13 +7,13 @@ ROOT="/home/wuyiming/git/Hand"
 TOOL=$ROOT/caffe/build/tools
 LOG=$ROOT/log/NYU
 CAFFEMODEL=$ROOT/weights/NYU
-MODELS=$ROOT/models/NYU/hand_baseline_copy
+MODELS=$ROOT/models/NYU/hand_baseline_deepprior_concate_features
 
 export PYTHONPATH=$ROOT/caffe/python:$ROOT/lib/data/:$ROOT/lib/data_layer:$ROOT/lib/util:$PYTHONPATH
 
-LOG_FILE="$LOG/baseline_copy_`date`.txt"
+LOG_FILE="$LOG/baseline_deepprior_concate_features_`date`.txt"
 exec &> >(tee -a "$LOG_FILE")
 echo Logging to "$LOG_FILE"
 
-$TOOL/caffe train -solver $MODELS/solver_hand_baseline_copy.prototxt \
-                  -gpu 1
+$TOOL/caffe train -solver $MODELS/solver_hand_baseline_deepprior_concate_features.prototxt \
+		  -gpu 1
--- experiments/NYU/train_lstm_multi_branches.sh
+++ experiments/NYU/train_lstm_multi_branches.sh
@@ -7,13 +7,14 @@ ROOT="/home/wuyiming/git/Hand"
 TOOL=$ROOT/caffe/build/tools
 LOG=$ROOT/log/NYU
 CAFFEMODEL=$ROOT/weights/NYU
-MODELS=$ROOT/models/NYU/hand_baseline_concate_features
+MODELS=$ROOT/models/NYU/hand_lstm_multi_branches
 
 export PYTHONPATH=$ROOT/caffe/python:$ROOT/lib/data/:$ROOT/lib/data_layer:$ROOT/lib/util:$PYTHONPATH
 
-LOG_FILE="$LOG/baseline_concate_features_`date`.txt"
+LOG_FILE="$LOG/lstm_multi_branches_`date`.txt"
 exec &> >(tee -a "$LOG_FILE")
 echo Logging to "$LOG_FILE"
 
-$TOOL/caffe train -solver $MODELS/solver_hand_baseline_concate_features.prototxt \
+$TOOL/caffe train -solver $MODELS/solver_hand_lstm_multi_branches.prototxt \
+		  -weights $CAFFEMODEL/hand_baseline/hand_baseline_iter_150000.caffemodel \
 		  -gpu 1
